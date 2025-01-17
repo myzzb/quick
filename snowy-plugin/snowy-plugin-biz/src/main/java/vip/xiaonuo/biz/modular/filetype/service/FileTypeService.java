d06274027388ae4425ddc6bd4abd3cbec99bce13
root <zzbxyz@163.com>
2025-01-17 17:49:41 +0800

    feat:新增文件资源类型模块

---
+++ snowy-plugin/snowy-plugin-biz/src/main/java/vip/xiaonuo/biz/modular/filetype/service/FileTypeService.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright [2022] [https://www.xiaonuo.vip]
+ *
+ * Snowy采用APACHE LICENSE 2.0开源协议，您在使用过程中，需要注意以下几点：
+ *
+ * 1.请不要删除和修改根目录下的LICENSE文件。
+ * 2.请不要删除和修改Snowy源码头部的版权声明。
+ * 3.本项目代码可免费商业使用，商业使用请保留源码和相关描述文件的项目出处，作者声明等。
+ * 4.分发源码时候，请注明软件出处 https://www.xiaonuo.vip
+ * 5.不可二次分发开源参与同类竞品，如有想法可联系团队xiaonuobase@qq.com商议合作。
+ * 6.若您的项目无法满足以上几点，需要更多功能代码，获取Snowy商业授权许可，请在官网购买授权，地址为 https://www.xiaonuo.vip
+ */
+package vip.xiaonuo.biz.modular.filetype.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import vip.xiaonuo.biz.modular.filetype.entity.FileType;
+import vip.xiaonuo.biz.modular.filetype.param.FileTypeAddParam;
+import vip.xiaonuo.biz.modular.filetype.param.FileTypeEditParam;
+import vip.xiaonuo.biz.modular.filetype.param.FileTypeIdParam;
+import vip.xiaonuo.biz.modular.filetype.param.FileTypePageParam;
+
+import java.util.List;
+
+/**
+ * 文件类型Service接口
+ *
+ * @author zzb
+ * @date  2025/01/17 15:04
+ **/
+public interface FileTypeService extends IService<FileType> {
+
+    /**
+     * 获取文件类型分页
+     *
+     * @author zzb
+     * @date  2025/01/17 15:04
+     */
+    Page<FileType> page(FileTypePageParam fileTypePageParam);
+
+    /**
+     * 添加文件类型
+     *
+     * @author zzb
+     * @date  2025/01/17 15:04
+     */
+    void add(FileTypeAddParam fileTypeAddParam);
+
+    /**
+     * 编辑文件类型
+     *
+     * @author zzb
+     * @date  2025/01/17 15:04
+     */
+    void edit(FileTypeEditParam fileTypeEditParam);
+
+    /**
+     * 删除文件类型
+     *
+     * @author zzb
+     * @date  2025/01/17 15:04
+     */
+    void delete(List<FileTypeIdParam> fileTypeIdParamList);
+
+    /**
+     * 获取文件类型详情
+     *
+     * @author zzb
+     * @date  2025/01/17 15:04
+     */
+    FileType detail(FileTypeIdParam fileTypeIdParam);
+
+    /**
+     * 获取文件类型详情
+     *
+     * @author zzb
+     * @date  2025/01/17 15:04
+     **/
+    FileType queryEntity(String id);
+}
---
+++ snowy-plugin/snowy-plugin-biz/src/main/java/vip/xiaonuo/biz/modular/filetype/service/impl/FileTypeServiceImpl.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright [2022] [https://www.xiaonuo.vip]
+ *
+ * Snowy采用APACHE LICENSE 2.0开源协议，您在使用过程中，需要注意以下几点：
+ *
+ * 1.请不要删除和修改根目录下的LICENSE文件。
+ * 2.请不要删除和修改Snowy源码头部的版权声明。
+ * 3.本项目代码可免费商业使用，商业使用请保留源码和相关描述文件的项目出处，作者声明等。
+ * 4.分发源码时候，请注明软件出处 https://www.xiaonuo.vip
+ * 5.不可二次分发开源参与同类竞品，如有想法可联系团队xiaonuobase@qq.com商议合作。
+ * 6.若您的项目无法满足以上几点，需要更多功能代码，获取Snowy商业授权许可，请在官网购买授权，地址为 https://www.xiaonuo.vip
+ */
+package vip.xiaonuo.biz.modular.filetype.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollStreamUtil;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import vip.xiaonuo.common.enums.CommonSortOrderEnum;
+import vip.xiaonuo.common.exception.CommonException;
+import vip.xiaonuo.common.page.CommonPageRequest;
+import vip.xiaonuo.biz.modular.filetype.entity.FileType;
+import vip.xiaonuo.biz.modular.filetype.mapper.FileTypeMapper;
+import vip.xiaonuo.biz.modular.filetype.param.FileTypeAddParam;
+import vip.xiaonuo.biz.modular.filetype.param.FileTypeEditParam;
+import vip.xiaonuo.biz.modular.filetype.param.FileTypeIdParam;
+import vip.xiaonuo.biz.modular.filetype.param.FileTypePageParam;
+import vip.xiaonuo.biz.modular.filetype.service.FileTypeService;
+
+import java.util.List;
+
+/**
+ * 文件类型Service接口实现类
+ *
+ * @author zzb
+ * @date  2025/01/17 15:04
+ **/
+@Service
+public class FileTypeServiceImpl extends ServiceImpl<FileTypeMapper, FileType> implements FileTypeService {
+
+    @Override
+    public Page<FileType> page(FileTypePageParam fileTypePageParam) {
+        QueryWrapper<FileType> queryWrapper = new QueryWrapper<FileType>().checkSqlInjection();
+        if(ObjectUtil.isNotEmpty(fileTypePageParam.getName())) {
+            queryWrapper.lambda().like(FileType::getName, fileTypePageParam.getName());
+        }
+        if(ObjectUtil.isAllNotEmpty(fileTypePageParam.getSortField(), fileTypePageParam.getSortOrder())) {
+            CommonSortOrderEnum.validate(fileTypePageParam.getSortOrder());
+            queryWrapper.orderBy(true, fileTypePageParam.getSortOrder().equals(CommonSortOrderEnum.ASC.getValue()),
+                    StrUtil.toUnderlineCase(fileTypePageParam.getSortField()));
+        } else {
+            queryWrapper.lambda().orderByAsc(FileType::getSortCode);
+        }
+        return this.page(CommonPageRequest.defaultPage(), queryWrapper);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void add(FileTypeAddParam fileTypeAddParam) {
+        FileType fileType = BeanUtil.toBean(fileTypeAddParam, FileType.class);
+        this.save(fileType);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void edit(FileTypeEditParam fileTypeEditParam) {
+        FileType fileType = this.queryEntity(fileTypeEditParam.getId());
+        BeanUtil.copyProperties(fileTypeEditParam, fileType);
+        this.updateById(fileType);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void delete(List<FileTypeIdParam> fileTypeIdParamList) {
+        // 执行删除
+        this.removeByIds(CollStreamUtil.toList(fileTypeIdParamList, FileTypeIdParam::getId));
+    }
+
+    @Override
+    public FileType detail(FileTypeIdParam fileTypeIdParam) {
+        return this.queryEntity(fileTypeIdParam.getId());
+    }
+
+    @Override
+    public FileType queryEntity(String id) {
+        FileType fileType = this.getById(id);
+        if(ObjectUtil.isEmpty(fileType)) {
+            throw new CommonException("文件类型不存在，id值为：{}", id);
+        }
+        return fileType;
+    }
+}
